51a6adffbba4d7e66ee3708f147383705fe971b4
ruSaendra <saendra.ru@gmail.com>
2021-02-05 18:38:56 +0300

    no message

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -14,8 +14,6 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            // string inputFilename = @"C:\Users\Saendra\Google Drive\ConsultationResult.docx";
-            // string outputFilename = @"C:\Users\Saendra\Google Drive\ConsultationResultEdited.docx";
             string inputFilename = @"ConsultationResult.docx";
             string outputFilenameDocX = @"ConsultationResultEditedDocX.docx";
             string outputFilenameXml = @"ConsultationResultEditedXml.docx";
